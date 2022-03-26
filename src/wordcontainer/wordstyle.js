deb12298bf3a8a3fcbf71c7364c7892f4e8c1d8d
shubham kumar <18shubham1998@gmail.com>
2022-03-27 04:49:11 +0530

    hoveronbutton

--- src/wordcontainer/wordstyle.js
+++ src/wordcontainer/wordstyle.js
@@ -7,4 +7,11 @@ export const Word_style=styled.button`
     border-radius:5px;
     padding:10px;
     box-shadow: rgba(50, 50, 93, 0.25) 0px 50px 100px -20px, rgba(0, 0, 0, 0.3) 0px 30px 60px -30px, rgba(10, 37, 64, 0.35) 0px -2px 6px 0px inset;
+
+
+    &:hover{
+        transform:scale(1.2);
+        background:#0de6d1;
+        color:#0a0aba;
+    }
 `
